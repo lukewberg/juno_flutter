1ad19dea84af8b3efd74d999897fa493b7915ade
Luke <lukewberg@gmail.com>
2022-07-29 11:13:37 -0700

    Some colors and dependencies

--- lib/components/juno_appbar.dart
+++ lib/components/juno_appbar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class JunoAppbar extends StatefulWidget {
   const JunoAppbar({Key? key}) : super(key: key);
@@ -10,21 +11,41 @@ class JunoAppbar extends StatefulWidget {
 class _JunoAppbarState extends State<JunoAppbar> {
   @override
   Widget build(BuildContext context) {
-    return DecoratedBox(decoration: const BoxDecoration(
-      color: Color(0xff271836),
-    ),
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: [
-        Container(
-          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-          child: const Text("Juno", style: TextStyle(
-              color: Colors.white
-          )),
-        )
-      ],
-    )
-    );
-
+    return DecoratedBox(
+        decoration: const BoxDecoration(
+            color: Color(0xff271836),
+            boxShadow: [BoxShadow(
+              color: Colors.grey,
+              blurRadius: 20,
+              spreadRadius: 1
+            )],
+            gradient: LinearGradient(
+                begin: Alignment.bottomLeft,
+                end: Alignment.topRight,
+                colors: [Color(0xff271836), Color(0xff6F67AE)])),
+        child: SafeArea(
+          top: true,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SvgPicture.asset(
+                      'assets/img/juno_logo_white.svg',
+                      height: 40,
+                      color: Colors.white,
+                    ),
+                    const Spacer(
+                      flex: 1,
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
