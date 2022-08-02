d27938b620fcf96da55ddcc9fac483cfa4ff7327
luke126 <luke@junolive.co>
2022-08-02 00:25:28 -0700

    WIP: Login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:juno_flutter/components/juno_appbar.dart';
 
@@ -12,11 +14,26 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      appBar:JunoAppbar(),
-      body: SingleChildScrollView(
-        child: Text("Hello world")
-      )
-    );
+        body: DecoratedBox(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.bottomLeft,
+                  end: Alignment.topRight,
+                  stops: [
+                    0.25,
+                    0.5,
+                    0.75
+                  ],
+                  colors: [
+                    Color(0xff6F67AE),
+                    Color(0xffBF2E92),
+                    Color(0xffE26B76)
+                  ]),
+            ),
+            child: SafeArea(
+              child: Center(
+                child: Text('Hello world!'),
+              ),
+            )));
   }
 }
-
